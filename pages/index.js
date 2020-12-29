b77b8be747dcbfaa2fe75b1e7b773efdee65e221
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-12-29 14:18:14 +0100

    deploy my next app

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,8 @@ import Employees from "./employees";
 import { FirebaseContext } from "../firebase";
 import Login from "./login";
 
-
 export default function Home() {
   const { user } = useContext(FirebaseContext);
-
 
   return (
     <>
