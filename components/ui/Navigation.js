94641bee9b809791ad0e6d5ee20ba333c78883f2
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-12-29 16:22:01 +0100

    Mejora en los estilos de las tablas y tamaño de letra en el header

--- components/ui/Navigation.js
+++ components/ui/Navigation.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { FirebaseContext } from "../../firebase";
 import styles from "../css/Navigation.module.scss"
 
-
 const Navigation = () => {
 
     const { user, firebase } = useContext(FirebaseContext);
@@ -11,10 +10,6 @@ const Navigation = () => {
     return (
         <nav className={styles.nav}>
             <Link href="/" ><a>Sindicato de la Carne</a></Link>
-            {/* <Link href="/employees/add">Agregar Empleado</Link> */}
-            {/* <Link className="nav-item" href="/">Inicio</Link>
-            <Link href="/employee/add">Agregar Empleado</Link>
-            <Link href="/company/add">Agregar Empresa</Link> */}
             {user ?
                 <form className={styles.form}>
                     <span>{user.displayName}</span>
@@ -40,37 +35,6 @@ const Navigation = () => {
                 </form>
             }
         </nav >
-
-        // <nav className="navbar navbar-dark bg-dark">
-        //     <Link href="/" className="navbar-brand"><a className={styles.a}>Sindicato de la Carne</a></Link>
-        //     {/* <Link href="/employees/add">Agregar Empleado</Link> */}
-        //     {/* <Link className="nav-item" href="/">Inicio</Link>
-        //     <Link href="/employee/add">Agregar Empleado</Link>
-        //     <Link href="/company/add">Agregar Empresa</Link> */}
-        //     {user ?
-        //         <form className="form-inline my-2 my-lg-0">
-        //             <span className="badge badge-pill badge-light">{user.displayName}</span>
-
-        //             <button
-        //                 className="btn btn-outline-success my-2 my-sm-0"
-        //                 type="button"
-        //                 onClick={() => firebase.cerrarSesion()}
-        //             >
-        //                 Cerrar Sesion
-        //                 </button>
-
-        //         </form>
-        //         :
-        //         <form className="form-inline my-2 my-lg-0">
-        //             <Link href="/login">
-        //                 <button className="btn btn-info" type="button">Login</button>
-        //             </Link>
-        //             <Link href="/signin">
-        //                 <button className="btn btn-danger" type="button">Crear cuenta</button>
-        //             </Link>
-        //         </form>
-        //     }
-        // </nav >
     );
 }
 
